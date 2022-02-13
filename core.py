c5f8d53555a598ad322aac7e5d2ef7a594cc9a8a
Diego Alberto Miquilena Gutiérrez <99613206+diegomiquilena@users.noreply.github.com>
2022-02-13 20:07:06 +0100

    Add files via upload

---
+++ core.py
@@ -0,0 +1,10 @@
+"""Fichero Principal"""
+import menu
+
+
+def main():
+    menu.loop()
+
+# Entendemos por esta condición que se ejecuta siempre y cuando el módulo sea principal.
+if __name__ == "__main__":
+    main()
---
+++ menu.py
@@ -0,0 +1,48 @@
+""" Menú del programa """
+import helpers
+import manager
+
+def loop():
+    while True:
+        
+        helpers.clear()
+
+        print("========================")
+        print("  BIENVENIDO AL GESTOR  ")
+        print("========================")
+        print("[1] Listar clientes     ")
+        print("[2] Mostrar cliente     ")
+        print("[3] Añadir cliente      ")
+        print("[4] Modificar cliente   ")
+        print("[5] Borrar cliente      ")
+        print("[6] Salir               ")
+        print("========================")
+
+        option = input("> ")
+
+        helpers.clear()
+
+        if option == '1':
+            print("Listando los clientes...\n")
+            manager.show_all()
+        if option == '2':
+            print("Mostrando un cliente...\n")
+            manager.find()
+        if option == '3':
+            print("Modificando un cliente...\n")
+            manager.add()
+            print("Cliente añadido con éxito")
+        if option == '4':
+            print("Modificando un cliente...\n")
+            if manager.edit():
+                print("Cliente modificado correctamente")
+        if option == '5':
+            print("Borrando un cliente...\n")
+            if manager.delete():
+                print("Cliente borrado correctamente")
+        if option == '6':
+            print("Saliendo...\n")
+            break
+
+
+        input("\nPresiona ENTER para continuar...")
